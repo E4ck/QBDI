93becd4a43887c5e873c67a8e48d23518e7f5663
Nicolas SURBAYROLE <nsurbayrole@quarkslab.com>
2019-05-02 16:22:48 +0200

    Windows In Progress

---
+++ cmake/config-win-X86.py
@@ -0,0 +1,9 @@
+import subprocess
+
+subprocess.check_call(["cmake", "..",
+                       "-G", "Visual Studio 16 2019",
+                       "-A", "Win32",
+                       "-DCMAKE_CXX_FLAGS=/D_SILENCE_TR1_NAMESPACE_DEPRECATION_WARNING",
+                       "-DCMAKE_BUILD_TYPE=Release",
+                       "-DCMAKE_CROSSCOMPILING=FALSE",
+                       "-DPLATFORM=win-X86"])
